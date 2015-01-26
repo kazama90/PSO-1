ba8fd76fbba6e972fe18da9b2c72a0c9f39a3a6d
netdis <netdisorder@gmail.com>
2015-01-26 02:26:40 +0100

    Klasa bazowa dla funkcji wielowymiarowych i ich konkretne implementacje
    Dodano abstrakcyjna klase bazowa dla funkcji i konkretne implementacje aby umozliwic obliczenie wartosci funkcji i wyznaczenie domeny.

---
+++ Projekt_PSO/PSO/Services/Interfaces/Function.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSO.Services.Interfaces
+{
+    public abstract class Function
+    {
+        protected Function(double[] x)
+        {
+            X = x;
+        }
+
+        public double[] X { get; protected set; }
+
+        public int Dimension
+        {
+            get
+            {
+                if (X == null)
+                    return 0;
+
+                return X.Length;
+            }
+
+        }
+
+        // Obliczenie wartosci danej funkcji
+        public abstract double Evaluate();
+
+        // Gorna i dolna granica domeny funkcji 
+        public abstract double LowerBound { get; }
+        public abstract double UpperBound { get; }
+
+        
+    }
+}
---
+++ Projekt_PSO/PSO/Services/Rosenbrock.cs
@@ -0,0 +1,42 @@
+﻿using PSO.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PSO.Services
+{
+    public class Rosenbrock : Function
+    {
+         public Rosenbrock(double[] x) : base(x) { }
+
+        // http://www.sfu.ca/~ssurjano/rosen.html
+        public override double Evaluate()
+        {
+            double sum = 0;
+            double leftBracket = 0;
+            double rightBracket = 0;
+
+            for(int i=0; i < Dimension - 1; i++)
+            {
+                leftBracket = Math.Pow(X[i + 1] - Math.Pow(X[i], 2), 2);
+                rightBracket = Math.Pow(X[i] - 1, 2);
+                sum += (100 * leftBracket) + rightBracket;
+            }
+
+            return sum;
+
+        }
+
+        public override double LowerBound
+        {
+            get { return -5; }
+        }
+
+        public override double UpperBound
+        {
+            get { return 10; }
+        }
+    }
+}
